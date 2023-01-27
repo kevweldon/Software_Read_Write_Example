5c5333341267009f0a8a56b4154650c0859ba9ab
Kevin Weldon <kevin.weldon@intel.com>
2023-01-27 09:13:06 -0500

    Commit

--- NiosV_Read_Write_Example_C/sys/sys_bb.v
+++
@@ -1,7 +0,0 @@
-module sys (
-		input  wire       clk_clk,         //      clk.clk
-		output wire [7:0] pio_8bit_export, // pio_8bit.export
-		input  wire       reset_reset      //    reset.reset
-	);
-endmodule
-
--- NiosV_Read_Write_Example_C/sys/sys_inst.v
+++
@@ -1,6 +0,0 @@
-	sys u0 (
-		.clk_clk         (_connected_to_clk_clk_),         //   input,  width = 1,      clk.clk
-		.pio_8bit_export (_connected_to_pio_8bit_export_), //  output,  width = 8, pio_8bit.export
-		.reset_reset     (_connected_to_reset_reset_)      //   input,  width = 1,    reset.reset
-	);
-
