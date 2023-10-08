af04377f84a8191034d710a7d07543b3ac9e8a1e
Kevin Weldon <kevin.weldon@intel.com>
2023-10-08 08:15:45 -0400

    Commit

--- JTAG_Read_Write_Example_TCL/cleanup.bat
+++ JTAG_Read_Write_Example_TCL/cleanup.bat
@@ -3,7 +3,7 @@ rmdir qdb tmp-clearbox software DNI /s /q
 rmdir .qsys_edit ip sys /s /q
 del *.rpt *.sof *.summary *.smsg *.pin *~ /s
 del *.qsf *.qpf *.qws *.v *.sv *.sdc *.done *.qsys /s
-del *.cdf *.sld *.qarlog *.legacy /s
+del *.cdf *.sld *.qarlog *.legacy *.log *.rec /s
 del *.json *.qdf *.xml #*# /s
 
 
